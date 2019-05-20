f56e912d797c0499a12b4db82ca448de386a261f
Chubbard022 <chubbard022@gmail.com>
2019-05-20 08:57:04 -0700

    authentication routes fleshed out, updating package.json

--- api/server.js
+++ api/server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const configureRoutes = require('../config/routes');
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const configureRoutes = require("../config/routes")
+const { authenticate } = require("../auth/authenticate");
 
-const server = express();
+//const sourceRouter = require("../projects/sourceRouter")
+//const categoryRouter = require("../projects/categoryRouter")
+//const RecipeRouter = require("../projects/recipeRouter")
+//const ingredientRouter = require("../projects/ingredientRouter")
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+
+//server.use("/api/source",sourceRouter)
+//server.use("/api/category",authenticate,categoryRouter)
+//server.use("/api/recipe",RecipeRouter)
+//server.use("/api/ingredient",ingredientRouter)
+
+server.get("/",(req,res)=>{
+    res.status(200).json("server working")
+})
 
 configureRoutes(server);
-module.exports = server;
-
-server.get('/', (req, res) => {
-  res.send(
-    'Server is alive! GET all users: /users , POST: /register , /login '
-  );
-});
-
-module.exports = server;
+module.exports = server
---
+++ config/routes.js
@@ -0,0 +1,62 @@
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { authenticate } = require("../auth/authenticate");
+const secret = require("../auth/secret").jwtKey;
+const Users = require("../auth/authenticate");
+
+module.exports = server => {
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+};
+
+function register(req, res) { 
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(users => {
+      res.status(201).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage:"error with registering"});
+    });
+}
+
+function login(req, res) {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ errorMessage: "Username and/or Password are incorrect" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({errorMessage:"ERROR with logging in"});
+    });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
