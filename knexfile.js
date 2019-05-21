a98b12b9b99e9e942d7df9af7948f031d0c224d1
Chubbard022 <chubbard022@gmail.com>
2019-05-21 08:46:54 -0700

    added pg support for heroku

--- knexfile.js
+++ knexfile.js
@@ -31,5 +31,15 @@ module.exports = {
       seeds:{
         directory:"./data/seeds"
     }
+  },
+  production:{
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+      migrations:{
+        directory:"./data/migrations"
+      },
+      seeds:{
+        directory:"./data/seeds"
+    }
   }
 };
