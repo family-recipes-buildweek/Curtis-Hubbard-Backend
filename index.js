7837c771a8e9e9f45d0387d9148efe7de17f4b0a
Chubbard022 <chubbard022@gmail.com>
2019-05-21 19:29:56 -0700

    route for uploading images up

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+import multerUploads from "./auth/multer"
 
 const server = require("./api/server")
+server.post("/upload",(req,res)=>{
+  console.log("req.body: ",req.body)
+})
 
 const port = process.env.PORT || 6500;
 server.listen(port, () => {
