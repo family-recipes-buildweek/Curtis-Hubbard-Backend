144df9c948ada613e61cc19fd5132d51e9107593
Chubbard022 <chubbard022@gmail.com>
2019-05-22 11:00:30 -0700

    testing for all routes started

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,11 +1,58 @@
 const request = require("supertest")
 const server = require("./server")
-
 describe("server.js",()=>{
     describe("GET /",()=>{
         it("should respond with 200 OK async with done", (done)=>{
             request(server)
                 .get("/")
+                .then(response=>{
+                    expect(response.status).toBe(200)
+                    done()
+                })
+        })
+    })
+})
+describe("ingredientRoute.js",()=>{
+    describe("GET /",()=>{
+        it("should respond with 200 OK async with done", (done)=>{
+            request(server)
+                .get("/api/ingredient")
+                .then(response=>{
+                    expect(response.status).toBe(200)
+                    done()
+                })
+        })
+    })
+})
+describe("categoryRoute.js",()=>{
+    describe("GET /",()=>{
+        it("should respond with 200 OK async with done", (done)=>{
+            request(server)
+                .get("/api/category")
+                .then(response=>{
+                    expect(response.status).not.toBe(200)
+                    done()
+                })
+        })
+    })
+})
+describe("recipeRoute.js",()=>{
+    describe("GET /",()=>{
+        it("should respond with 200 OK async with done", (done)=>{
+            request(server)
+                .get("/api/recipe")
+                .then(response=>{
+                    expect(response.status).toBe(200)
+                    done()
+                })
+        })
+    })
+})
+describe("sourceRoute.js",()=>{
+    describe("GET /",()=>{
+        it("should respond with 200 OK async with done", (done)=>{
+            request(server)
+                .get("/api/recipe")
                 .then(response=>{
                     expect(response.status).toBe(200)
                     done()
